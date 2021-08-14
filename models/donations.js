5904196af037d830d44e10e175b58146c8ccdf9d
Ryan <ryanscar9@gmail.com>
2021-08-14 17:07:21 +0800

    updated donations.js file

--- models/donations.js
+++ models/donations.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const donationSchema = new Schema({
     donator_id: {
         type: Schema.Types.ObjectId,
-        ref: "Donation", //refer to the Donation model
+        ref: "Users", //refer to the User model
     },
     program_id: {
         type: Schema.Types.ObjectId,
