0c06ecc7effa1ef4887d85fc6da194e49b11a647
Ryan <ryanscar9@gmail.com>
2021-08-15 17:11:07 +0800

    added post request to create programs (fundraiser

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { Manager } = require("./managers/manager");
 const { Controller } = require("./controllers/controller");
 const connectDB = require("./config/db");
 
+// Models
+const Program = require("./models/programs");
+const Donation = require("./models/donations");
+const Notification = require("./models/notifications");
+const User = require("./models/users");
+
 const express = require("express");
 
 //MongoDB Atlas Connection
@@ -23,6 +29,26 @@ function login(req, res) {
     controller.login(req, res);
 }
 app.post("/login", login);
+
+// fundraiser create program
+app.post("/:id/programs", async (req, res) => {
+    const { id } = req.params; //fundraiser (USER) id
+
+    const { title, description } = req.body;
+
+    const newProgram = new Program({
+        title,
+        description,
+        collected_amount: 0,
+        fundraiser_id: id,
+        isVerified: false,
+    });
+
+    await newProgram.save();
+
+    res.send(newProgram);
+    // res.redirect("/")
+});
 
 app.listen(port, () => {
     console.log(`NINJA-BE is running on port: ${port}`);
