759f996dbb3b7aed743c0b67716f41264eabca7f
Ryan <ryanscar9@gmail.com>
2021-08-15 16:24:59 +0800

    required dotenv and DB connection in index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
+
 const { Repository } = require("./repositories/repository");
 const { Manager } = require("./managers/manager");
 const { Controller } = require("./controllers/controller");
+const connectDB = require("./config/db");
 
 const express = require("express");
+
+//MongoDB Atlas Connection
+connectDB();
 
 const app = express();
 const port = 3000;
@@ -13,8 +19,8 @@ const repository = new Repository();
 const manager = new Manager(repository);
 const controller = new Controller(manager);
 
-function login(req, res){
-    controller.login(req, res)
+function login(req, res) {
+    controller.login(req, res);
 }
 app.post("/login", login);
 
